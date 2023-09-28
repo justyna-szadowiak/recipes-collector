1cf98b8a66f444ae2949d5fa4d7d7d9fc14e3589
Justyna <justyna.elzbieta.szadowiak@gmail.com>
2023-09-28 14:03:06 +0200

    Initialize project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ],
+            },
+        ]
+    }
+};
+
+module.exports = config;
